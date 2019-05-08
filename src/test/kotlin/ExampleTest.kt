c5770f5a46e4646b16664fe9cb31382614cc3a71
Marcin Jędraszczyk <marcin.jedraszczyk@cognifide.com>
2019-05-08 09:55:05 +0200

    Improved formatting.

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,12 +1,14 @@
 import com.cognifide.aem.apmt.PermissionTest
 
 class ExampleTest : PermissionTest({
+
     "add assets" {
         test = JavaTest::pathContainsUser
         addPath("/content/sites/author")
         addUser(Users.GLOBAL_AUTHOR)
         addUser(Users.PL_AUTHOR)
     }
+
     "modify assets" {
         test = JavaTest::alwaysSuccess
         addPath("/content/sites/site")
