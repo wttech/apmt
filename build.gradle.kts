792a747a30d3d5f6ff9fa437871133e3f81556b8
Marcin Jędraszczyk <marcin.jedraszczyk@cognifide.com>
2019-05-08 12:58:31 +0200

    Build configured to run tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
