c96e3216c1fb84615ad78cec5abce31625b7e274
Marcin Jędraszczyk <marcin.jedraszczyk@cognifide.com>
2019-08-12 15:35:00 +0200

    Removed redundant class.

--- examples/src/test/kotlin/com/cognifide/apmt/exampleenum/KotlinEnumTest.kt
+++
@@ -1,5 +0,0 @@
-package com.cognifide.apmt.exampleenum
-
-import com.cognifide.apmt.tests.asset.CreateAssetsTest
-
-class KotlinEnumTest : CreateAssetsTest(KotlinTestCases.ADD_ASSETS.toTestCaseConfiguration())
